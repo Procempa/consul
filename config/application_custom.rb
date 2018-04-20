602f67feb25203865a42dc0c5b555ecbfd75541e
Tiago P. Zini <tiagozini@tiagozini-002794vm.procempa.com.br>
2018-04-20 17:06:03 -0300

    Modificações iniciais de idioma e icone

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = 'pt-BR'
+    config.i18n.available_locales = ['pt-BR', :es, :en]        
+    config.time_zone = 'Brasilia'
   end
 end
