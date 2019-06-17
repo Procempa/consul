fde6aa7d049a5df604d0175bfa7d567bdaffffb5
Tiago P. Zini <tiagozini@gmail.com>
2019-06-17 17:20:00 -0300

    Adjusting send email settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   }
   config.action_mailer.perform_deliveries = true  
 
+  # Deliver emails to a development mailbox at /letter_opener
+  config.action_mailer.delivery_method = :smtp
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
