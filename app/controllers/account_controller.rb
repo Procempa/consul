627ad95f97b6b52b733cb922926899228a0fb820
Tiago P. Zini <tiagozini@gmail.com>
2019-06-17 15:23:13 -0300

    Ajust in the moderation email notification

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -44,10 +44,10 @@ class AccountController < ApplicationController
 
     def account_params
       attributes = if @account.organization?
-                     [:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter,
+                     [:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter, :email_on_proposal_moderation,
                       organization_attributes: [:name, :responsible_name]]
                    else
-                     [:username, :public_activity, :public_interests, :email_on_comment,
+                     [:username, :public_activity, :public_interests, :email_on_comment, :email_on_proposal_moderation, 
                       :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter,
                       :official_position_badge, :document_type, :document_number, address_attributes: [:street, :postal_code, :neighbourhood, :number, :complement, :id] ]
                    end
